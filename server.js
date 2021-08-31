631e48419e5ef74b1b5ddee37cb0366758039a38
Carlos Escobar <escobarcarlos@gmail.com>
2021-08-31 14:46:54 -0600

    Second-Commit

--- server.js
+++ server.js
@@ -16,6 +16,25 @@ app.get('/', (req,res)=> {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('Information sent successfully')
 })
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Studen added Successfully', {author:'Carlos'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
+})
 
 const port = process.env.PORT || 4903
 
